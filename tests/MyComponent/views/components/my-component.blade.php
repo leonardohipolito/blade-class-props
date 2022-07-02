123696e8fbc2c4634c4c9d98da46a25dac1d481f
Leonardo Hipolito <leonardohipolito@outlook.com>
2022-07-02 12:23:30 -0300

    feat: add support to callable props

--- tests/MyComponent/views/components/my-component.blade.php
+++ tests/MyComponent/views/components/my-component.blade.php
@@ -1,4 +1,12 @@
 @props(['size'=>false])
-<button {{$attributes->classProps(['xs'=>'button-xs','md'=>'button-md'],'md')}}>
+<button {{$attributes
+    ->classProps([
+        'xs'=>fn($c)=>$c->has('outline')?'button-xs-outline':'button-xs',
+        'md'=>'button-md'
+    ],'md')
+    ->classProps([
+        'outline'=>'bg-transparent'
+    ])
+}}>
     {{$slot}}
 </button>
